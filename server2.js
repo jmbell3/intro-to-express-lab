0b6976a75db9752801a25697901e5c7e028bd25d
jmbell3 <168394340+jmbell3@users.noreply.github.com>
2024-07-10 22:10:11 -0400

    Add files via upload

--- server2.js
+++ server2.js
@@ -6,12 +6,12 @@ const port = 3002;
 
 app.get('/roll/:number',(req,res)=>{
     const numberParam = req.params.number;
-    const maxNumber = parseInt(numberParam,10);
+    const maxNumber = parseInt(numberParam,10); // converts the string paramter to an interger 
 
     if(isNaN(maxNumber)){
         res.send("You must specify a number.");
     }else{
-        const rolledNumber = Math.floor(Math.random()*(maxNumber + 1));
+        const rolledNumber = Math.floor(Math.random()*(maxNumber + 1)); //math.floor rounds up/ math.random() // give random number *(maxNumber + 1 // will give you a whole number
         res.send(`you rolled a ${rolledNumber}`);
     }
 });
